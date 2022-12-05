3a8bbce9239838ac8a42d335519cbbef00674657
Tianyan Zhou <melody.zhou@mail.utoronto.ca>
2022-12-04 22:10:24 -0500

    the function name of data_pre is changed to adjust

--- R/adjust.R
+++ R/adjust.R
@@ -1,4 +1,4 @@
-#' Data_pre
+#' adjust
 #'
 #' This function is used to pre-process the data for regression.
 #' For variables that require categorical analysis, this function factors out the variable.
@@ -6,17 +6,17 @@
 #' This function will return the changed data frame
 #' @param ln_name Variable you want to ln
 #' @param fc_name Variable you want to treat as factor
-#' @param data Data waitted ti be compute
+#' @param data Data waited to be computed
 #'
-#' @return A transformed dataframe
+#' @return A transformed data frame
 #' @export
 #' @examples data("test")
 #' ln_name <- c("AGE")
 #' fc_name <- c("ASTHMA")
 #' data <- test
-#' data_pre(ln_name,fc_name,data)
+#' adjust(ln_name,fc_name,data)
 #'
-data_pre <- function(ln_name,fc_name,data){
+adjust <- function(ln_name,fc_name,data){
 
   data[,ln_name] <- purrr::map_dfc(data[,ln_name],log)
   data[,fc_name] <- purrr::map_dfc(data[,fc_name],factor)
