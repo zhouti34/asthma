0b74fb054290af044dcbdea21aaafe962fb4956f
Tianyan Zhou <melody.zhou@mail.utoronto.ca>
2022-12-04 22:32:41 -0500

    description is modified and different Roxygen tags are separated.

--- R/adjust.R
+++ R/adjust.R
@@ -2,14 +2,18 @@
 #'
 #' This function is used to pre-process the data for regression.
 #' For variables that require categorical analysis, this function factors out the variable.
-#' If the data distribution is too skewed, log improvement is used.
+#' log improvement is used for the skewed data distribution.
 #' This function will return the changed data frame
 #' @param ln_name Variable you want to ln
 #' @param fc_name Variable you want to treat as factor
 #' @param data Data waited to be computed
+#'
 #' @import purrr
+#'
 #' @return A transformed data frame
+#'
 #' @export
+#'
 #' @examples data("test")
 #' ln_name <- c("AGE")
 #' fc_name <- c("ASTHMA")
